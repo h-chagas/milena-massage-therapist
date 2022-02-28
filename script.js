284785f469a4e6c7b0e42313737ffd0034d28b31
Hugo Chagas <75392085+h-chagas@users.noreply.github.com>
2022-02-28 19:31:45 +0000

    1

--- script.js
+++ script.js
@@ -67,3 +67,33 @@ function hbMenuIcon() {
         }
     
 }
+
+//Milena Massage Therapist fade out while srolling - id="milenaName"
+
+var header = document.getElementById('milenaName');
+
+function fadeOutOnScroll(element) {
+	if (!element) {
+		return;
+	}
+	
+	var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+	var elementHeight = element.offsetHeight;
+	var scrollTop = document.documentElement.scrollTop;
+	
+	var opacity = 1;
+	
+	if (scrollTop > distanceToTop) {
+		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+	}
+	
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
+}
+
+function scrollHandler() {
+	fadeOutOnScroll(header);
+}
+
+window.addEventListener('scroll', scrollHandler);
